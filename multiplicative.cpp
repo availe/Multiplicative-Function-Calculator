26ffb78b0d0c5482e0064957e51c5e28c730685f
availe <94802525+availe@users.noreply.github.com>
2024-12-02 23:42:36 -0500

    euler's totient and tau function

--- multiplicative.cpp
+++ multiplicative.cpp
@@ -16,36 +16,33 @@ int Multiplicative_Functions::sum_of_divisors(int n) {
     for (const auto& [prime, exponent] : prime_factors) {
         const int numerator = static_cast<int>(pow(prime, exponent + 1) - 1);
         const int denominator = prime - 1;
-        sum_of_divisors += numerator / denominator;
+        sum_of_divisors *= numerator / denominator;
     }
 
     return sum_of_divisors;
 }
 
-/** eulers_totient *= 1 - (1 / prime);
+/** eulers_totient equals n ∏ [1 - (1 / prime)]
  *
- * We must avoid using floating point numbers in C++ so we'll rephrase it
- *
- * 1prime - 1 = euler's totient / prime
- * (prime - 1) / prime = euler's totient
+ * But we must take floating numbers and integer division into account with C++
+ * φ(n) = n ∏ [1 - (1/p)]
+ * φ(n) = φ(n) [1 - (1/p)]
+ * φ(n) = φ(n) [(p-1)/p]
+ * φ(n) = [φ(n)(p-1)]/p
  **/
 
 int Multiplicative_Functions::eulers_totient(int n) {
     int eulers_totient = n;
     for (const auto& [prime, exponent] : prime_factors) {
-        eulers_totient *= (prime - 1) / prime;
+        eulers_totient = (eulers_totient * (prime - 1)) / prime;
     }
 
     return eulers_totient;
 }
 
 int Multiplicative_Functions::tau_function(int n) {
-    int tau_result = 1;
-
-    for (const auto& [prime, exponent] : prime_factors) {
-
-    }
-    return tau_result;
+    const size_t num_distinct_prime_factors = prime_factors.size();
+    return static_cast<int>(pow(-1, num_distinct_prime_factors));
 }
 
 
